9b9407813e47044a3b1e20b612be234bd376b15d
Jon Graca <jon.graca@libertymutual.com>
2017-12-05 09:04:27 -0500

    Incorporating Feedback and changing if/ continue to else if

--- src/main/java/com/lmig/gfc/romannumerals/services/HinduToRomanNumeralConverter.java
+++ src/main/java/com/lmig/gfc/romannumerals/services/HinduToRomanNumeralConverter.java
@@ -11,67 +11,42 @@ public class HinduToRomanNumeralConverter {
 			if (input >= 1000) {
 				highRoman += "M";
 				input -= 1000;
-				continue;
-			}
-			if (input >= 900) {
+			} else if (input >= 900) {
 				highRoman += "CM";
 				input -= 900;
-				continue;
-			}
-			if (input >= 500) {
+			} else if (input >= 500) {
 				highRoman += "D";
 				input -= 500;
-				continue;
-			}
-			if (input >= 400) {
+			} else if (input >= 400) {
 				highRoman += "CD";
 				input -= 400;
-				continue;
-			}
-			if (input >= 100) {
+			} else if (input >= 100) {
 				highRoman += "C";
 				input -= 100;
-				continue;
-			}
-			if (input >= 90) {
+			} else if (input >= 90) {
 				highRoman += "XC";
 				input -= 90;
-				continue;
-			}
-			if (input >= 50) {
+			} else if (input >= 50) {
 				highRoman += "L";
 				input -= 50;
-				continue;
-			}
-			if (input >= 40) {
+			} else if (input >= 40) {
 				highRoman += "XL";
 				input -= 40;
-				continue;
-			}
-			if (input >= 10) {
+			} else if (input >= 10) {
 				highRoman += "X";
 				input -= 10;
-				continue;
-			}
-			if (input >= 9) {
+			} else if (input >= 9) {
 				highRoman += "IX";
 				input -= 9;
-				continue;
-			}
-			if (input >= 5) {
+			} else if (input >= 5) {
 				highRoman += "V";
 				input -= 5;
-				continue;
-			}
-			if (input >= 4) {
+			} else if (input >= 4) {
 				highRoman += "IV";
 				input -= 4;
-				continue;
-			}
-			if (input >= 1) {
+			} else if (input >= 1) {
 				highRoman += "I";
 				input -= 1;
-				continue;
 			}
 		}
 		return highRoman;
